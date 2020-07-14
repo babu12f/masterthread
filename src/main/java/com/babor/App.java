5b402b0a8be2dd96c2a3a4c2fed029bc8b82daa4
Shofiullah Babor <babu_12f@yahoo.com>
2020-07-14 19:25:49 +0600

    thread master initial commit

---
+++ src/main/java/com/babor/App.java
@@ -0,0 +1,14 @@
+package com.babor;
+
+/**
+ * Master Thread
+ *
+ * @author Shofiullah Babor
+ * @email babu_12f@yahoo.com
+ *
+ */
+public class App {
+    public static void main( String[] args ) {
+
+    }
+}
