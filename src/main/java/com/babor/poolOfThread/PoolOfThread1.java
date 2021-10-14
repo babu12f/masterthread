ffc11215f2a0a9363f538a2f22fe59d621407a59
Shofiullah Babor <babu_12f@yahoo.com>
2021-10-15 01:17:56 +0600

    Executor service thread pool

---
+++ src/main/java/com/babor/poolOfThread/PoolOfThread1.java
@@ -0,0 +1,47 @@
+package com.babor.poolOfThread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+class Processor implements Runnable {
+
+    private int id;
+
+    public Processor(int id) {
+        this.id = id;
+    }
+
+    public void run() {
+        System.out.println("Starting: " + id);
+        try {
+            Thread.sleep(5000);
+        }
+        catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        System.out.println("Completed: " + id);
+    }
+}
+
+public class PoolOfThread1 {
+
+    public static void main(String[] args) {
+        /**
+         * Created 2 threads, and assign tasks (Processor(i).run) to the threads
+         */
+        ExecutorService executor = Executors.newFixedThreadPool(2);//2 Threads
+        for (int i = 0; i < 5; i++) { // call the (Processor(i).run) 2 times with 2 threads
+            executor.submit(new Processor(i));
+        }
+        executor.shutdown();
+        System.out.println("All tasks submitted.");
+        try {
+            executor.awaitTermination(1, TimeUnit.HOURS);
+        }
+        catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+        System.out.println("All tasks completed.");
+    }
+}
