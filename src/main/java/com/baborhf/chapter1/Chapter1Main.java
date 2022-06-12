2c19dad3fee7e2e5604fd9e20af6450395be1e1a
Shofiullah Babor <babu_12f@yahoo.com>
2022-06-12 20:30:49 +0600

    fc-1

---
+++ src/main/java/com/baborhf/chapter1/Chapter1Main.java
@@ -0,0 +1,7 @@
+package com.baborhf.chapter1;
+
+public class Chapter1Main {
+    public static void main(String[] args) {
+        System.out.println("Head first Java. 2nd Edition. java - 5.0");
+    }
+}
